ba6ba6726c83168cb86c20999116948d4e1322ff
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-06-27 23:39:20 -0700

    Get login/logout working (#133)
    * Get login/logout working
    
    * Remove error log

--- js/index.ts
+++ js/index.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import "spectre.css/src/spectre.scss";
+import createAuth0Client from "@auth0/auth0-spa-js";
+import Auth0Client from "@auth0/auth0-spa-js/dist/typings/Auth0Client";
 import * as Sentry from "@sentry/browser";
 import { register } from "register-service-worker";
 
@@ -7,10 +9,72 @@ Sentry.init({
   dsn: "https://046b94f8170f4135a47ca9d0f9709a6d@sentry.io/1438468"
 });
 
+/**
+ * Fire event that can be handled by Seed application
+ *
+ * @param {string} msgName Msg variant name, e.g. Tick(String) will be "Tick"
+ * @param {*|undefined} data Serialized Msg variant data
+ */
+function triggerUpdate(msgName: string, msgData?: unknown): void {
+  const event = new CustomEvent("triggerupdate", {
+    detail: msgData === undefined ? msgName : { [msgName]: msgData }
+  });
+  window.dispatchEvent(event);
+}
+
+let auth0: Auth0Client;
+
+async function checkAuth(): Promise<boolean> {
+  const isAuthenticated = await auth0.isAuthenticated();
+  triggerUpdate("Authenticated", isAuthenticated);
+  if (isAuthenticated) {
+    const token = await auth0.getTokenSilently();
+    const user = await auth0.getUser();
+  }
+  return isAuthenticated;
+}
+
+async function authenticate() {
+  auth0 = await createAuth0Client({
+    domain: "d20.auth0.com",
+    client_id: "IjRu7XqVRtUIEOjwhzC7Bbe2P1zRVhPC"
+  });
+
+  const isAuthenticated = await checkAuth();
+  if (isAuthenticated) return;
+
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+    // Process the login state
+    await auth0.handleRedirectCallback();
+    await checkAuth();
+    // Use replaceState to redirect the user away and remove the querystring parameters
+    window.history.replaceState({}, document.title, "/");
+  }
+}
+
+async function login() {
+  await auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+  });
+}
+
+function logout() {
+  auth0.logout({
+    returnTo: window.location.origin
+  });
+}
+
+// @ts-ignore
+window.d20Login = login;
+// @ts-ignore
+window.d20Logout = logout;
+
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
 import("../pkg").then((module): void => {
   module.render();
+  authenticate();
 });
 
 register("/service-worker.js", {
