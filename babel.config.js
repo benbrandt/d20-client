25ce0aac725eb5697cda60d2290ecf088b89425b
Ben Brandt <benjamin.j.brandt@gmail.com>
2019-07-04 09:55:59 -0700

    Update babel

--- babel.config.js
+++ babel.config.js
@@ -1,45 +1,41 @@
 module.exports = function(api) {
   api.cache(false);
-
   const env = process.env.NODE_ENV;
-  const presets = [
-    env === "test"
-      ? [
-          "@babel/preset-env",
-          {
-            corejs: 3,
-            shippedProposals: true,
-            targets: {
-              node: "current"
-            },
-            useBuiltIns: "usage"
-          }
-        ]
-      : [
-          "@babel/preset-env",
-          {
-            corejs: 3,
-            modules: false,
-            shippedProposals: true,
-            useBuiltIns: "usage"
-          }
-        ],
-    "@babel/preset-typescript"
-  ];
-  const plugins = [
-    "@babel/plugin-syntax-dynamic-import",
-    [
-      "@babel/plugin-transform-runtime",
-      {
-        regenerator: false,
-        useESModules: env !== "test"
-      }
-    ]
-  ];
 
   return {
     sourceType: "unambiguous",
-    presets,
-    plugins
+    presets: [
+      env === "test"
+        ? [
+            "@babel/preset-env",
+            {
+              corejs: 3,
+              shippedProposals: true,
+              targets: {
+                node: "current"
+              },
+              useBuiltIns: "usage"
+            }
+          ]
+        : [
+            "@babel/preset-env",
+            {
+              corejs: 3,
+              modules: false,
+              shippedProposals: true,
+              useBuiltIns: "usage"
+            }
+          ],
+      "@babel/preset-typescript"
+    ],
+    plugins: [
+      [
+        "@babel/plugin-transform-runtime",
+        {
+          regenerator: false,
+          useESModules: env !== "test"
+        }
+      ]
+    ]
   };
 };
